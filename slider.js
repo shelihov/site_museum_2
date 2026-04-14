799b63a96e0aa65ba2080960b04c4dae00a2c451
shelihov <schetelih@gmail.com>
2026-04-14 21:58:45 +0300

    cлайдер готов на 75%

---
+++ slider.js
@@ -0,0 +1,56 @@
+const slider = document.querySelector('.slider');
+
+if (slider) {
+    const sliderImage = slider.querySelector('.slider-image');
+    const sliderDescription = slider.querySelector('.slider-description');
+    const prevBtn = slider.querySelector('.slider-prev');
+    const nextBtn = slider.querySelector('.slider-next');
+
+    const images = JSON.parse(slider.dataset.images);
+    const descriptions = JSON.parse(slider.dataset.descriptions);
+
+    const modal = document.getElementById("sliderModal");
+    const modalImage = document.getElementById("sliderModalImage");
+    const modalClose = document.getElementById("sliderModalClose");
+
+    let currentIndex = 0;
+
+    // Показать текущую картинку и описание фото
+    function showImage() {
+        sliderImage.src = images[currentIndex];
+        sliderImage.alt = descriptions[currentIndex];
+
+        modalImage.src = images[currentIndex];
+        modalImage.alt = descriptions[currentIndex];
+
+        sliderDescription.textContent = descriptions[currentIndex];
+    }
+
+    nextBtn.addEventListener('click', function () {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage();
+    });
+
+    prevBtn.addEventListener('click', function () {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage();
+    });
+
+    sliderImage.addEventListener('click', function () {
+        modal.classList.add('open');
+        modalImage.src = images[currentIndex];
+        modalImage.alt = descriptions[currentIndex];
+    });
+
+    modalClose.addEventListener('click', function () {
+        modal.classList.remove('open');
+    });
+
+    modal.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.classList.remove('open');
+        }
+    });
+
+    showImage();
+}

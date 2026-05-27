f296ff853d21179fb11dda111dfd39852ebf26ab
shelihov <schetelih@gmail.com>
2026-05-28 02:20:22 +0300

    Поменяны все относительные пути! для деплоя на GitHUb

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+const siteRoot = new URL("./", document.currentScript.src);
+
+function resolveRootPaths(container) {
+  container.querySelectorAll("a[href^='/'], img[src^='/']").forEach((element) => {
+    const attribute = element.hasAttribute("href") ? "href" : "src";
+    const value = element.getAttribute(attribute);
+    element.setAttribute(attribute, new URL(value.slice(1), siteRoot).href);
+  });
+}
+
  // Загружаем содержимое header.html в элемент с id "header"
 const headerContainer = document.getElementById("header");
 
 if (headerContainer) {
-  fetch("/header.html")
+  fetch(new URL("header.html", siteRoot))
     .then((response) => response.text())
     .then((data) => {
       headerContainer.innerHTML = data;
+      resolveRootPaths(headerContainer);
 
       const burgerBtn = document.getElementById("burgerBtn");
       const mainNav = document.getElementById("mainNav");
@@ -22,9 +33,10 @@ if (headerContainer) {
 const footerContainer = document.getElementById("footer");
 
 if (footerContainer) {
-  fetch("/footer.html")
+  fetch(new URL("footer.html", siteRoot))
   .then((response) => response.text())
   .then((data) => {
     footerContainer.innerHTML = data;
+    resolveRootPaths(footerContainer);
   })
 }
